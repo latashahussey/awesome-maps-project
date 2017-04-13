b15e9a3f64ffe73f10092ba55bcbdf6ed211443f
sugarcookie <latasha.hussey@gmail.com>
2017-04-13 14:22:45 -0500

    Create functions for buttons to show/hide listings

--- src/js/app.js
+++ src/js/app.js
@@ -24,49 +24,70 @@ function initMap() {
         {title: 'South Lamar', location: {lat: 30.236265, lng: -97.782422}}
     ];
 
+
+
     // To hold data in info window for marker
     var largeInfoWindow = new google.maps.InfoWindow();
 
-    // In case our markers move outside boundaries of original maps location
-    var bounds = new google.maps.LatLngBounds();
-
-    // Loop through all of the locations
     for (var i = 0; i < locations.length; i++){
         // Get the position and title of the location array
         var position = locations[i].location;
         var title = locations[i].title;
         //Create a maker per position, and put markers into array
         var marker = new google.maps.Marker({
-            map: map,
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
             id: i
         });
+
         // Push the markers to the array of markers
         markers.push(marker);
-        // Extend the boundaries of the map each marker
-        bounds.extend(marker.position);
+
         // Create an onclick even to open an infowindow at each marker.
         marker.addListener('click', function() {
             populateInfoWindow(this, largeInfoWindow);
         });
     }
+
+    // When clicked, run appropriate function to show or hide listings
+    document.getElementById('show-listings').addEventListener('click', showListings);
+    document.getElementById('hide-listings').addEventListener('click', hideListings);
+}
+
+
+// This function populuates the infowindow when a marker is clicked
+// Only one infowindow will open at a time.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened at this marker.
+    if(infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        //Make sure the marker property is closed if the infowindow is closed.
+        infowindow.addListener('closeclick', function(){
+            infowindow.setMarker(null);
+        });
+    }
+}
+
+
+function showListings() {
+    // In case our markers move outside boundaries of original maps location
+    var bounds = new google.maps.LatLngBounds();
+    // Loop through all of the locations
+    for(var i = 0; i < markers.length; i++){
+        markers[i].setMap(map); // now set the map
+        // Extend the boundaries of the map each marker
+        bounds.extend(markers[i].position);
+    }
     // Fit map to bounds
     map.fitBounds(bounds);
+}
 
-    // This function populuates the infowindow when a marker is clicked
-    // Only one infowindow will open at a time.
-    function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened at this marker.
-        if(infowindow.marker != marker) {
-            infowindow.marker = marker;
-            infowindow.setContent('<div>' + marker.title + '</div>');
-            infowindow.open(map, marker);
-            //Make sure the marker property is closed if the infowindow is closed.
-            infowindow.addListener('closeclick', function(){
-                infowindow.setMarker(null);
-            });
-        }
+function hideListings() {
+    // hide all the markers
+    for(var i = 0; markers.length; i++) {
+        markers[i].setMap(null);
     }
 }
